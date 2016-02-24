4d52bd978699c7af6f73fd09950422489e20279f
Larry Lade <lplade@users.noreply.github.com>
2016-02-24 13:09:16 -0600

    15 line web server!

---
+++ helloNode.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lade on 2/24/16.
+ */
+var http = require('http');
+var server = http.createServer(function(request, response) {
+
+	console.log(request.headers);
+	console.log(request.method);
+	console.log(request.url);
+
+	response.statusCode = 200;
+	response.setHeader('Content-Type', 'text/html');
+
+	response.write('<html><body><h1>My first server!</h1></body></html>');
+	response.end();
+
+}).listen(8080);
